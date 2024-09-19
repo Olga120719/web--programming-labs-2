44ea583d278b9d30f85d1e5daaed3abb93bffbff
Цой Ольга <tsoi.0lga@yandex.ru>
2024-09-19 21:16:44 +0700

    Меню

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return '''
 <!doctype html>
@@ -22,4 +23,67 @@ def start():
     </body>
 </html>
 '''
+
+@app.route("/menu")
+def menu():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ ФБ Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <main>
+
+        <ol>
+            <li><a href="http://127.0.0.1:5000/lab1">Первая лабораторная</a></li>
+        <ol>
+
+        </main>
+
+        <footer>
+            &copy; Цой Ольга, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        
+        <title>Цой Ольга Дмитриевна, Лабораторная работа 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+        <p>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов Werkzeug,
+            а также шаблонизатор Jinja2. Относится к категории так называемых
+            микрофреймворков — минималистичных каркасов веб-приложений,
+            сознательно предоставляющих лишь самые базовые возможности.
+        </p>
+        
+        <p>
+            <a href="http://127.0.0.1:5000/menu">Меню</a>
+        </p>
+
+        <footer>
+            &copy; Цой Ольга, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+'''
+
 
