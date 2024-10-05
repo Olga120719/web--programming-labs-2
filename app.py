301db3ccb14d5e2f7c790ea4722d3a5af345efe6
Цой Ольга <tsoi.0lga@yandex.ru>
2024-10-05 16:43:22 +0700

    Готово

--- app.py
+++ app.py
@@ -3,8 +3,37 @@ app = Flask(__name__)
 
 @app.route('/lab2/example')
 def example():
-       name = "ФБИ-23"
-       return render_template('example.html', name=name)
+    name, number, group, course = 'Цой Ольга', 2, 'ФБИ-23', 3
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+        ]
+    books = [
+        {'author': 'Лев Толстой', 'title': 'Война и мир', 'genre': 'Исторический роман', 'pages': 1200},
+        {'author': 'Федор Достоевский', 'title': 'Преступление и наказание', 'genre': 'Роман', 'pages': 600},
+        {'author': 'Антуан де Сент-Экзюпери', 'title': 'Маленький принц', 'genre': 'Фантазия', 'pages': 100},
+        {'author': 'Габриэль Гарсиа Маркес', 'title': 'Сто лет одиночества', 'genre': 'Магический реализм', 'pages': 450},
+        {'author': 'Джордж Оруэлл', 'title': '1984', 'genre': 'Дистопия', 'pages': 328},
+        {'author': 'Достоевский Ф.', 'title': 'Идиот', 'genre': 'Роман', 'pages': 700},
+        {'author': 'Гарри Поттер', 'title': 'Гарри Поттер и философский камень', 'genre': 'Фэнтези', 'pages': 223},
+        {'author': 'Джейн Остин', 'title': 'Гордость и предубеждение', 'genre': 'Роман', 'pages': 432},
+        {'author': 'Марк Твен', 'title': 'Приключения Гекльберри Финна', 'genre': 'Приключения', 'pages': 366},
+        {'author': 'Джон Р. Р. Толкин', 'title': 'Властелин колец', 'genre': 'Фэнтези', 'pages': 1178}
+        ]
+    return render_template('example.html', name=name, number=number, 
+    group=group, course=course, fruits=fruits, books=books)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
+
+@app.route('/lab2/memes/')
+def memes():
+    return render_template('memes.html')
+
 
 @app.route("/")
 @app.route("/index")
@@ -28,6 +57,7 @@ def menu():
         <main>
             <ol>
                 <li><a href="/lab1">Первая лабораторная</a></li>
+                <li><a href="/lab2">Вторая лабораторная</a></li>
             </ol>
         </main>
 
